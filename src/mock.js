3a5d1d38ad33517369d978421373bb5dc7c504ba
trevonerd <info@djtrevo.com>
2023-07-07 08:29:35 +0200

    removed unused file

--- src/mock.js
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { RemoteSVGMock } = require('./dist');
-
-jest.mock('react-remote-svg', () => ({
-  RemoteSVG: RemoteSVGMock,
-}));
